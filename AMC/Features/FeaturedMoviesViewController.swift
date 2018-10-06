7fd00cfa133ddba6bb608ccaa4f92628557c58c2
Bruno Agatte <bruno.agatte@backstage.com>
2018-10-06 14:31:02 -0400

    project folder structure created

---
+++ AMC/Features/FeaturedMoviesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeaturedMoviesViewController.swift
+//  AMC
+//
+//  Created by Bruno Agatte on 10/6/18.
+//  Copyright © 2018 Bruno Agatte. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
